7358c9cb1de197a8c58753cfec269e0f3aff0674
Bodleum <bodleum.phone@gmail.com>
2020-04-09 13:31:21 +0100

    Initial commit

---
+++ GDP Sim.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+class Ball:
+    def __init__(self,x0,y0):
+
+        self.mass=0
+
+        self.radius=0
+
+        #x
+        self.ax=0
+        self=vx=0
+        self.x=x0
+
+        #y
+        self.ay=0
+        self.vy=0
+        self.y=y0
+        
