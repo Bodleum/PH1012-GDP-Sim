d6423c5ef7b83621d8d416cb09ccb6a4be986f74
Bodleum <bodleum.phone@gmail.com>
2020-04-09 14:16:49 +0100

    Added animated graph

--- GDP Sim.py
+++ GDP Sim.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import animation
+from matplotlib.animation import FuncAnimation
 
 gravity=9.81
 airdens = 1.2041  # from Wikipedia -> temporary
-t=0
 tmax=float(input("Maximum time? --> "))
 tstep=0.01
+ux=10
+uy=15
 class Ball:
     def __init__(self,x0,y0):
 
@@ -36,14 +37,41 @@ class Ball:
 
         #x
         self.ax=0
-        self.vx=self.ax*t
-        self.x=0.5*self.ax*t**2
+        self.vx=self.ax*t+ux
+        self.x=0.5*self.ax*t**2+ux*t
 
         #y
         self.ay=-1*gravity
-        self.vy=self.ay*t
-        self.y=0.5*self.ay*t**2
+        self.vy=self.ay*t+uy
+        self.y=0.5*self.ay*t**2+uy*t
 
         #Air resistance
         self.airres = self.drag*0.5*(np.pi*self.radius**2)*self.vx**2
-        
+
+
+golf=Ball(0,0)
+
+x_data=[]
+y_data=[]
+
+fig, ax=plt.subplots()
+ax.set_xlim(-10,tmax+0.05*tmax)
+ax.set_ylim(-20,20)
+line, =ax.plot(0,0)
+
+def animation_frame(i):
+
+    #Update golf
+    golf.update(i)
+
+    #Append to x_data and y_data
+    x_data.append(golf.x)
+    y_data.append(golf.y)
+
+    #Add to line
+    line.set_xdata(x_data)
+    line.set_ydata(y_data)
+    return line,
+
+animation = FuncAnimation(fig,func=animation_frame,frames=np.arange(0,tmax,tstep),interval=10)
+plt.show()
