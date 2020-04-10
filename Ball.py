a57db654847f2ba9360c97523196a4fe7adb85ff
Bodleum <bodleum.phone@gmail.com>
2020-04-10 12:08:22 +0100

    Updated velocity system of Ball class

--- Ball.py
+++ Ball.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from GDP_Sim import gravity,airdens,vinit,x_data,y_data 
+from GDP_Sim import gravity,airdens,vinit,x_data,y_data
 
 class Ball:
     def __init__(self, x0, y0,dragcoef):
@@ -15,42 +15,37 @@ class Ball:
 
         #x
         self.ax = None
-        self.vx = None
         self.x = x0
 
         #y
         self.ay = None
-        self.vy = None
         self.y = y0
 
-        #Initial velocities
-        self.ux = vinit.x
-        self.uy = vinit.y
+        #Initial velocity
+        self.vel=vinit
 
         #Air resistance
         self.drag = dragcoef
-        self.airresx = airdens*self.drag*0.5*(np.pi*self.radius**2)*self.vx**2
-        self.airresy = airdens*self.drag*0.5*(np.pi*self.radius**2)*self.vy**2
+        self.airresx = airdens*self.drag*0.5*(np.pi*self.radius**2)*self.vel.x**2
+        self.airresy = airdens*self.drag*0.5*(np.pi*self.radius**2)*self.vel.y**2
 
     def update(self, t):
 
         #x
         self.ax = (self.airresx/self.mass)
-        self.vx = self.ax*t+self.ux
-        self.x = 0.5*self.ax*t**2+self.ux*t
+        self.vel.x += self.ax
+        self.x += self.vel.x
         x_data.append(self.x)  # Append to graph data
 
         #y
         self.ay = -1*gravity + (self.airresy/self.mass)
-        self.vy = self.ay*t+self.uy
-        self.y = 0.5*self.ay*t**2+self.uy*t
+        self.vel.y += self.ay
+        self.y += self.vel.y
         y_data.append(self.y)  # Append to graph data
 
         #Air resistance
-        self.airresx = -1*airdens*self.drag * \
-            0.5*(np.pi*self.radius**2)*self.vx**2
-        self.airresy = -1*airdens*self.drag * \
-            0.5*(np.pi*self.radius**2)*self.vy**2
+        self.airresx = -1*airdens*self.drag *0.5*(np.pi*self.radius**2)*self.vel.x**2
+        self.airresy = -1*airdens*self.drag *0.5*(np.pi*self.radius**2)*self.vel.y**2
 
         #Set ux and uy for next update
         # self.ux=self.vx
