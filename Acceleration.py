c693cad3919eb2d3e1d7c093f3b2b177ae350d44
Bodleum <bodleum.phone@gmail.com>
2020-04-12 18:54:39 +0100

    Created acceleration class

---
+++ Acceleration.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Acceleration:
+    def __init__(self,mag,degangle):
+        self.mag = mag
+        self.degangle = degangle
+        self.radangle = (self.degangle*np.pi)/180
+        self.x = self.mag*np.cos(self.radangle)
+        self.y = self.mag*np.sin(self.radangle)
