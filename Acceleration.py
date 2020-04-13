30325bc26f8377d1aa92e83fdfbb9eed7c67282a
Bodleum <bodleum.phone@gmail.com>
2020-04-13 09:04:02 +0100

    Added names to acceleration and velocity

--- Acceleration.py
+++ Acceleration.py
@@ -2,7 +2,8 @@ import numpy as np
 
 class Acceleration:
     
-    def __init__(self,mag,degangle):
+    def __init__(self,name,mag,degangle):
+        self.name=name
         self.mag = mag
         self.degangle = degangle
         self.radangle = (self.degangle*np.pi)/180
