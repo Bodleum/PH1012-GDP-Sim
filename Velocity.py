eb82a53897807d4c4a069014853c18f705368337
Bodleum <bodleum.phone@gmail.com>
2020-04-13 09:27:03 +0100

    Added functionality to velocity class

--- Velocity.py
+++ Velocity.py
@@ -6,6 +6,22 @@ class Velocity:
         self.name=name
         self.mag = mag
         self.degangle = degangle
+        self.resolve()
+
+    def resolve(self):
         self.radangle = (self.degangle*np.pi)/180
         self.x = self.mag*np.cos(self.radangle)
         self.y = self.mag*np.sin(self.radangle)
+
+    def add(self,vel2):
+        self.mag += vel2.mag
+        self.degangle += vel2.degangle
+        if self.degangle>180:
+            self.degangle += -360
+        elif self.degangle<=(-180):
+            self.degangle += 360
+
+        self.resolve()
+    
+    def rename(delf,rename):
+        self.name = renames
