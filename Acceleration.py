90f4f9127c5f0e6c41a27037cc50797731a947d1
Bodleum <bodleum.phone@gmail.com>
2020-04-13 13:29:08 +0100

    Massively improved

--- Acceleration.py
+++ Acceleration.py
@@ -3,24 +3,58 @@ import numpy as np
 class Acceleration:
     
     def __init__(self,name,mag,degangle):
-        self.name=name
+        self.name = name
         self.mag = mag
-        self.degangle = degangle
+        if degangle > 180:
+            self.degangle = degangle - 360
+        elif degangle <= -180:
+            self.degangle = degangle + 360
+        else:
+            self.degangle = degangle
+        self.resolve()
 
     def resolve(self):
         self.radangle = (self.degangle*np.pi)/180
         self.x = self.mag*np.cos(self.radangle)
         self.y = self.mag*np.sin(self.radangle)
+        if 0 <= self.degangle and 90 >= self.degangle:  # Quad 1
+            self.x = np.abs(self.x)
+            self.y = np.abs(self.y)
+        elif 90 < self.degangle and 180 >= self.degangle:  # Quad 2
+            self.x = -1*np.abs(self.x)
+            self.y = np.abs(self.y)
+        elif -90 > self.degangle and -180 < self.degangle:  # Quad 3
+            self.x = -1*np.abs(self.x)
+            self.y = -1*np.abs(self.y)
+        elif 0 > self.degangle and -90 <= self.degangle:  # Quad
+            self.x = np.abs(self.x)
+            self.y = -1*np.abs(self.y)
+
+        self.mag = np.sqrt(self.x**2+self.y**2)
+        self.radangle = np.arctan2(self.y, self.x)
+        self.degangle = self.radangle * 180/np.pi
 
     def add(self, accel2):
-        self.mag += accel2.mag
-        self.degangle += accel2.degangle
-        if self.degangle > 180:
-            self.degangle += -360
-        elif self.degangle <= (-180):
-            self.degangle += 360
+        self.x += accel2.x
+        self.y += accel2.y
 
-        self.resolve()
+        self.mag = np.sqrt(self.x**2+self.y**2)
+        self.radangle = np.arctan2(self.y, self.x)
+        print(self.radangle)
+        self.degangle = self.radangle * 180/np.pi
+
+        if 0 <= self.degangle and 90 >= self.degangle:  # Quad 1
+            self.x = np.abs(self.x)
+            self.y = np.abs(self.y)
+        elif 90 < self.degangle and 180 >= self.degangle:  # Quad 2
+            self.x = -1*np.abs(self.x)
+            self.y = np.abs(self.y)
+        elif -90 > self.degangle and -180 < self.degangle:  # Quad 3
+            self.x = -1*np.abs(self.x)
+            self.y = -1*np.abs(self.y)
+        elif 0 > self.degangle and -90 <= self.degangle:  # Quad
+            self.x = np.abs(self.x)
+            self.y = -1*np.abs(self.y)
 
     def rename(self, rename):
         self.name = rename
